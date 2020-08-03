53c25ad88a8efe93ee2ba86635ac11bd17af0b6c
Gubin Daniil <danilych919191@gmail.com>
2020-08-03 23:56:03 +0300

    При обращении в лс бот выдает расписание и цену(без базы данных)

--- vk_bot.py
+++ vk_bot.py
@@ -3,12 +3,22 @@ from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from vk_api.utils import get_random_id
 try:
     from group_token import token
-except Exception as err:
-    print('Ошибка токена')
+except ImportError as err:
+    token = None
+    print(f'Ошибка токена, токен = {token}')
+    print(err)
+try:
+    from data_base import timetable
+except ImportError as err:
+    timetable = None
+    print('Ошибка расписания')
     print(err)
 
 
 GROUP_ID = 197388508
+
+price_words_list = ['Цена', 'цена', 'Стоимость', 'стоимость']
+timetable_words_list = ['Расписание', 'расписание']
 
 
 class Bot:
@@ -19,12 +29,51 @@ class Bot:
 
     def on_event(self):
         for event in self.long_poll.listen():
-            if event.type == VkBotEventType.WALL_POST_NEW and abs(event.object.from_id) == GROUP_ID:
-                for user_id in self.vk_api.groups.getMembers(group_id=GROUP_ID, filter='friends')['items']:
-                    self.vk_api.messages.send(user_id=user_id, random_id=get_random_id(), peer_id=GROUP_ID,
-                                              message=event.object.text)
+            self.send_post_from_wall_to_members_ls(event=event)
+            self.get_info_from_bot_in_messages(event=event)
+
+    def send_post_from_wall_to_members_ls(self, event):
+        if event.type == VkBotEventType.WALL_POST_NEW and abs(event.object.from_id) == GROUP_ID:
+            for user_id in self.vk_api.groups.getMembers(group_id=GROUP_ID, filter='friends')['items']:
+                self.vk_api.messages.send(user_id=user_id,
+                                          random_id=get_random_id(),
+                                          peer_id=GROUP_ID,
+                                          message=event.object.text)
+
+    def get_info_from_bot_in_messages(self, event):
+        if event.type == VkBotEventType.MESSAGE_NEW:
+            if event.object.text in timetable_words_list:
+                self.vk_api.messages.send(user_id=event.object.from_id,
+                                          random_id=get_random_id(),
+                                          peer_id=GROUP_ID,
+                                          message=self.data_base_format_to_message())
+
+            elif event.object.text in price_words_list:
+                self.vk_api.messages.send(user_id=event.object.from_id,
+                                          random_id=get_random_id(),
+                                          peer_id=GROUP_ID,
+                                          message='''
+                                          Первое групповое занятие - бесплатно
+                                          Цена за групповое занятие - 500 рублей
+                                          Персональная тренировка - 1000 рублей
+                                          ''')
             else:
-                print(f'Событие типа {event.type} еще не обрабатываются')
+                self.vk_api.messages.send(user_id=event.object.from_id,
+                                          random_id=get_random_id(),
+                                          peer_id=GROUP_ID,
+                                          message='''
+                                          Я вас не понимаю =(
+                                          'Цена' - чтобы узнать стоимость занятия
+                                          'Расписание' - чтобы узнать расписание тренировок
+                                          ''')
+
+    @staticmethod
+    def data_base_format_to_message():
+        result_answer = ''
+        for key, value in timetable.items():
+            result_answer += f'{key} : {value} \n'
+
+        return result_answer
 
 
 if __name__ == '__main__':
